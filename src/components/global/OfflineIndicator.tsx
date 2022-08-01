8dc10d0599254a2890097385542cd97c1bdf6acb
boedegoat <bhremada.fka@gmail.com>
2022-08-01 16:31:46 +0700

    add offline indicator

---
+++ src/components/global/OfflineIndicator.tsx
@@ -0,0 +1,21 @@
+import { ExclamationIcon } from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
+
+const OfflineIndicator = () => {
+    const [isOffline, setIsOffline] = useState(false);
+
+    useEffect(() => {
+        if (!navigator.onLine) {
+            setIsOffline(true);
+        }
+    }, []);
+
+    return isOffline ? (
+        <div className="px-5 py-3 flex items-center justify-center text-orange-700 bg-orange-100">
+            <ExclamationIcon className="w-5 h-5 mr-2" />
+            <span className="font-medium text-lg">You are offline</span>
+        </div>
+    ) : null;
+};
+
+export default OfflineIndicator;
